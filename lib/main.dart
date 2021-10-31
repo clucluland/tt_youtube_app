30e19b1c41f5b7bb08b403c969eebc5a14d8f66a
TETSUYA-PCK2\tetsuya <tetsuya_tada@fandmnet.com>
2021-10-31 23:20:07 +0900

    一旦保存

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,61 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.white),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('TT Youtube Sample App'),
+          centerTitle: false,
+          leading: const Icon(Icons.videocam),
+          title: const Text(
+            'KBOYのFlutter大学',
+          ),
+          actions: <Widget>[
+            SizedBox(
+              width: 44,
+              child: IconButton(
+                onPressed: () {
+                  // todo
+                },
+                icon: const Icon(Icons.search),
+              ),
+            ),
+            SizedBox(
+              width: 44,
+              child: IconButton(
+                onPressed: () {
+                  // todo
+                },
+                icon: const Icon(Icons.more_vert),
+              ),
+            ),
+          ],
         ),
-        body: Container(),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Image.network(
+                  'https://icooon-mono.com/i/icon_11354/icon_113541_64.png'),
+              Column(
+                children: <Widget>[
+                  const Text(
+                    'KBOYのFlutter大学',
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      // todo
+                    },
+                    child: Row(
+                      children: const <Widget>[
+                        Icon(Icons.video_call),
+                        Text('登録する'),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
